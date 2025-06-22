09d2522e39f44f13200e18b8715a8b6b70f12847
hasandg <hasan.dag@nar.com.tr>
2025-06-23 00:51:08 +0300

    repo-queries-made-case-sensitive

--- mongo_vs_postgres_server/src/main/java/com/hasandag/mongovspostgres/model/MongoProduct.java
+++ mongo_vs_postgres_server/src/main/java/com/hasandag/mongovspostgres/model/MongoProduct.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
 
 @Document(collection = "products")
-@CompoundIndex(name = "idx_category_name", def = "{category: 1, name: 1}", collation = "{ locale: 'en', strength: 2 }")
+@CompoundIndex(name = "idx_category_name", def = "{category: 1, name: 1}")
 public class MongoProduct {
     @Id
     private String id;
