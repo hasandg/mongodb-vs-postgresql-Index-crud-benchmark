c98bd25ba3d0a5cbc01abdfe483642fa9192a248
hasandg <hasan.dag@nar.com.tr>
2025-06-23 13:20:32 +0300

    delete-made-bulk

--- mongo_vs_postgres_server/src/main/java/com/hasandag/mongovspostgres/repository/MongoProductRepository.java
+++ mongo_vs_postgres_server/src/main/java/com/hasandag/mongovspostgres/repository/MongoProductRepository.java
@@ -8,12 +8,8 @@ import java.util.List;
 
 @Repository
 public interface MongoProductRepository extends MongoRepository<MongoProduct, String> {
-    List<MongoProduct> findByNameAndCategory(String name, String category);
-    List<MongoProduct> findByName(String name);
-    List<MongoProduct> findByCategory(String category);
 
-    // Partial match / LIKE equivalent
-    List<MongoProduct> findByNameStartingWithIgnoreCase(String name);
-    List<MongoProduct> findByCategoryStartingWithIgnoreCase(String category);
-    List<MongoProduct> findByNameStartingWithIgnoreCaseAndCategoryStartingWithIgnoreCase(String name, String category);
+    List<MongoProduct> findByNameStartingWith(String name);
+    List<MongoProduct> findByCategoryStartingWith(String category);
+    List<MongoProduct> findByCategoryAndName(String category, String name);
 } 
