43321697412192961fd0882563b3fd7e2750c070
hasandg <hasan.dag@nar.com.tr>
2025-06-26 00:00:56 +0300

    index imports removed

--- mongo_vs_postgres_server/src/main/java/com/hasandag/mongovspostgres/model/MongoProduct.java
+++ mongo_vs_postgres_server/src/main/java/com/hasandag/mongovspostgres/model/MongoProduct.java
@@ -1,6 +1,5 @@
 package com.hasandag.mongovspostgres.model;
 
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.annotation.Id;
 
